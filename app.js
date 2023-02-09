8a262123c4527bd45a6f4e90440ae23c4be34e87
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-02-09 15:18:24 +0800

    first commit

--- app.js
+++ app.js
@@ -146,7 +146,10 @@ app.post("/", function(req, res){
     });
 });
 
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(3000, function() {
-    console.log("The Server is running at port 3000");
+    console.log("The Server is running successfully");
 })
